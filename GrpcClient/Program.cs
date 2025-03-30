e2a7629164691b2eeb1927dbd498885ca03cba83
hlw422 <hlw422@126.com>
2025-03-30 19:47:37 +0800

    1.grpc 客户端和服务端

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GrpcClient;
+using Grpc.Net.Client;
+class Program
+{
+    async static Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:5162");
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(
+            new HelloRequest { Name = "GreeterClient" });
+        Console.WriteLine("Greeting: " + reply.Message);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
